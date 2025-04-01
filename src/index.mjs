b152e820adcbe57886842904c026d39d47ab92d7
Dataquark <daniil.razlivanov99@gmail.com>
2025-04-01 19:02:43 +0300

    get request

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,22 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.get('/', (req, res) => {
+  res.status(201).send({ msg: 'Hello' });
+});
+
+app.get('/api/users', (req, res) => {
+  res.send([
+    { id: 1, username: 'anson', displayName: 'Anson' },
+    { id: 2, username: 'jack', displayName: 'Jack' },
+    { id: 3, username: 'adam', displayName: 'Adam' },
+  ]);
+});
+
+app.get('/api/products', (req, res) => {
+  res.send([{ id: 123, name: 'chicken breast', price: 12.99 }]);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running in PORT: ${PORT}`);
 });
