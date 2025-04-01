a65f37ece8dde87ab3ef516f75d9651e89b7ded8
Dataquark <daniil.razlivanov99@gmail.com>
2025-04-01 19:11:02 +0300

    route parameters

--- src/index.mjs
+++ src/index.mjs
@@ -4,16 +4,28 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+const mockUsers = [
+  { id: 1, username: 'anson', displayName: 'Anson' },
+  { id: 2, username: 'jack', displayName: 'Jack' },
+  { id: 3, username: 'adam', displayName: 'Adam' },
+];
+
 app.get('/', (req, res) => {
   res.status(201).send({ msg: 'Hello' });
 });
 
 app.get('/api/users', (req, res) => {
-  res.send([
-    { id: 1, username: 'anson', displayName: 'Anson' },
-    { id: 2, username: 'jack', displayName: 'Jack' },
-    { id: 3, username: 'adam', displayName: 'Adam' },
-  ]);
+  res.send(mockUsers);
+});
+
+app.get('/api/users/:id', (req, res) => {
+  const parsedId = parseInt(req.params.id);
+  if (isNaN(parsedId)) return res.status(400).send({ msg: 'Bad request. Invalid ID' });
+
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser) return res.sendStatus(404);
+
+  return res.send(findUser);
 });
 
 app.get('/api/products', (req, res) => {
